71cf35438593f959c3ef34cc4c512f9d66f1d24a
fercgodoi <mariafefe17022015@gmail.com>
2020-06-11 18:03:14 -0300

    first8966799ffsdf

--- controllers/teste/teste-controller.js
+++ controllers/teste/teste-controller.js
@@ -4,7 +4,7 @@ const mysql = require('../../mysql').pool;
 exports.SelectAgendamento = (req, res, next) => {
     mysql.getConnection((error, conn) =>{
         if(error){return res.json({ error: error})}          
-        conn.query('desc funcionario',
+        conn.query('select * from funcionario where idFunc=3',
             (error, resultado, field)=> {
                 if(error){return res.json({ error: error})} 
                 return res.json({resultado})
